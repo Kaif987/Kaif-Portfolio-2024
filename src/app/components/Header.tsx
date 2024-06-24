142be49c4c21a91ade2f6330ae59e38bc92995ee
Mohammad Kaif <mdkaifprofession@gmail.com>
2024-06-24 13:48:44 +0545

    Add Navbar

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,45 @@
+'use client'
+import { usePathname } from "next/navigation";
+import { ThemeToggle } from "./theme-toggle";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+
+export default function Header() {
+    const PAGES = [
+        {
+            name: "Home",
+            href: "/"
+        },
+        {
+            name: "Projects",
+            href: "/projects"
+        },
+        {
+            name: "Writings",
+            href: "/writings"
+        },
+        {
+            name: "Contact",
+            href: "/contact"
+        }
+    ]
+
+    const pathname = usePathname()
+
+    const isActive = (href: string) => href === pathname
+
+    return (
+        <nav className="flex items-center justify-between px-20 py-4">
+            <ul className="flex gap-4">
+                {PAGES.map(page => {
+                    return <Link href={page.href} className={cn("text-sm",
+                        isActive(page.href) && "font-bold"
+                    )}>
+                        {page.name}
+                    </Link>
+                })}
+            </ul>
+            <ThemeToggle />
+        </nav>
+    )
+}

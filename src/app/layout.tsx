fca782a26485a165178b43a5ed204faff592c89a
Mohammad Kaif <mdkaifprofession@gmail.com>
2024-07-20 17:40:29 +0545

    Add analytics to website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "./components/theme-provider";
 import Header from "./components/Header";
 import { Footer } from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -35,6 +36,7 @@ export default function RootLayout({
             <Footer />
           </div>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
